9f5c1d9bd14114917d3252544d42b7aa3174c1ec
Cody Darst <cody.darst@gmail.com>
2018-01-09 15:48:23 -0600

    Fixed PHP Fatal error.  Implemented ArrayAccess methods to allow array operator access to class properties.  fixes NagiosEnterprises/nagiosvshell#45

--- www/api/application/libraries/factory/NagiosObject.php
+++ www/api/application/libraries/factory/NagiosObject.php
@@ -100,5 +100,23 @@ abstract class NagiosObject extends StdClass
 		return $this->_type;
 	}
 
+	public function offsetExists ($offset)
+	{
+		return isset($this->$offset);
+	}
 
+	public function offsetGet ($offset)
+	{
+		return $this->$offset;
+	}
+
+	public function offsetSet ($offset, $value)
+	{
+		$this->$offset = $value;
+	}
+
+	public function offsetUnset ($offset)
+	{
+		unset($this->$offset);
+	}
 }
